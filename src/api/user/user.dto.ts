9aad44983f1796b0de783b5765b09f441b2c7654
Saeed <sa.ghofraniivari@gmail.com>
2022-05-16 21:22:04 +0430

    Update user DTO

--- src/api/user/user.dto.ts
+++ src/api/user/user.dto.ts
@@ -1,10 +1,37 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateUserDto {
+    
+    @ApiProperty({ default: 'user' })
     @IsString()
     @IsNotEmpty()
     public name: string;
 
+    @ApiProperty({ default: '1234' })
+    @IsNotEmpty()
+    password: string;
+
+    @ApiProperty({ description: 'Valid email.', default: 'test@test.com' })
     @IsEmail()
-    public email: string;
+    @IsNotEmpty()
+    email: string;
+
+}
+
+export class UpdateUserDto {
+
+    @ApiProperty({ required: false })
+    @IsOptional()
+    name: string;
+
+    @ApiProperty({ required: false })
+    @IsOptional()
+    password: string;
+
+    @ApiProperty({ required: false, description: 'Valid email.' })
+    @IsEmail()
+    @IsOptional()
+    email: string;
+
 }
