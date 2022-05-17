5f53d74c9a0965f38aea7d18c8618fa38a89a677
Saeed <sa.ghofraniivari@gmail.com>
2022-05-17 09:09:00 +0430

    add task

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [UserModule, AuthModule]
+  imports: [UserModule, AuthModule, TaskModule]
 })
 export class ApiModule { }
 
