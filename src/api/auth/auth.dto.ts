4f3999120cc1d2e812b2d91a16ccdeb2d8774409
Saeed <sa.ghofraniivari@gmail.com>
2022-05-17 10:17:12 +0430

    unnecessary code

--- src/api/auth/auth.dto.ts
+++ src/api/auth/auth.dto.ts
@@ -4,12 +4,10 @@ import { IsNotEmpty, Length } from 'class-validator';
 export class LoginDto {
 
   @ApiProperty({ required: true, default: 'user' })
-  @Length(3, 20)
   @IsNotEmpty()
   name: string;
 
   @ApiProperty({ required: true, default: '1234' })
-  @Length(8, 20)
   @IsNotEmpty()
   password: string;
 
