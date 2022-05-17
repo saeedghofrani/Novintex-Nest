921fb12e884bec80dfe00e2ec9fe1593b1a07794
Saeed <sa.ghofraniivari@gmail.com>
2022-05-17 10:16:24 +0430

    add authentication for Task api

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
 import { ApiModule } from './api/api.module';
 import { AuthorizationMiddleware } from './api/auth/auth.middleware';
 import { AuthModule } from './api/auth/auth.module';
+import { TaskModule } from './api/task/task.module';
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
@@ -17,6 +18,7 @@ const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
     ApiModule,
     UserModule,
     AuthModule,
+    TaskModule
 
   ],
   controllers: [],
@@ -26,7 +28,7 @@ const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(AuthorizationMiddleware).forRoutes('user');
-    // consumer.apply(AuthorizationMiddleware).forRoutes('task');
+    consumer.apply(AuthorizationMiddleware).forRoutes('task');
     consumer.apply(AuthorizationMiddleware).forRoutes('auth/logout');
   }
 }
