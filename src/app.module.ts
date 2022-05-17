d622de27e0cfe58d49b71d89b4fb76b16499f4b6
Saeed <sa.ghofraniivari@gmail.com>
2022-05-17 14:35:36 +0430

    .env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,12 +10,24 @@ import { AuthModule } from './api/auth/auth.module';
 import { TaskModule } from './api/task/task.module';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import * as Joi from 'joi';
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    ConfigModule.forRoot({
+      envFilePath,
+      isGlobal: true,
+      cache: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+      }),
+    }),
+
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
     ThrottlerModule.forRoot({
       ttl: 60,
